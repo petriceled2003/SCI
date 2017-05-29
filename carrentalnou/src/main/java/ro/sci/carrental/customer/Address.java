1dd9251446aa48cce8e2e602b93544f8e7ff0d53
Roronoa <petriceled2003@gmail.com>
2017-05-29 14:01:34 +0300

    Added class Address

---
+++ carrentalnou/src/main/java/ro/sci/carrental/customer/Address.java
@@ -0,0 +1,58 @@
+package ro.sci.carrental.customer;
+
+import java.sql.Struct;
+
+/**
+ * Created by Roronoa on 5/29/2017.
+ */
+public class Address {
+    private String name;
+    private String surname;
+    private String city;
+    private String street;
+    private String number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+
+
+
+}
