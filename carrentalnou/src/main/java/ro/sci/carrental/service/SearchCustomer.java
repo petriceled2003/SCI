e351f7a69f920e672ff6f4b397967c497af1cc65
Darius <petriceled2003@gmail.com>
2017-06-06 13:38:23 +0300

    Created class SearchCusotmerImpl that implements interface SearchCustomer.

--- carrentalnou/src/main/java/ro/sci/carrental/service/SearchCustomer.java
+++ carrentalnou/src/main/java/ro/sci/carrental/service/SearchCustomer.java
@@ -7,6 +7,7 @@ import java.util.List;
 /**
  * Created by Roronoa on 6/6/2017.
  */
+
 public interface SearchCustomer {
     List<Customer> findCustomerByName(String name);
 
--- carrentalnou/src/main/java/ro/sci/carrental/service/SearchCustomerImpl.java
+++ carrentalnou/src/main/java/ro/sci/carrental/service/SearchCustomerImpl.java
@@ -7,6 +7,7 @@ import java.util.List;
 /**
  * Created by Roronoa on 6/6/2017.
  */
+
 public class SearchCustomerImpl implements SearchCustomer {
     public List<Customer> findCustomerByName(String name) {
         return null;
