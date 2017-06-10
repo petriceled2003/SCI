5c7ef9ac4e79d5980450ae9905034a3eed5cdabf
Darius <petriceled2003@gmail.com>
2017-06-10 19:12:12 +0300

    Added Transaction Interface created car and customer package under domain

---
+++ carrentalnou/src/main/java/ro/sci/carrental/service/SearchCustomer.java
@@ -0,0 +1,18 @@
+package ro.sci.carrental.service;
+
+import ro.sci.carrental.domain.customer.Customer;
+
+import java.util.List;
+
+/**
+ * Created by Roronoa on 6/6/2017.
+ */
+
+public interface SearchCustomer {
+    List<Customer> findCustomerByName(String name);
+
+    List<Customer> findCustomerByNameAndCity(String name, String city);
+
+    List<Customer> findCustomerByMultipleCategories(String name, String citty  , String street, int number);
+}
+
---
+++ carrentalnou/src/main/java/ro/sci/carrental/service/SearchServiceImpl.java
@@ -0,0 +1,82 @@
+package ro.sci.carrental.service;
+
+import ro.sci.carrental.domain.car.Car;
+import ro.sci.carrental.repository.CarRepositoryImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SearchServiceImpl implements SearchService {
+
+    private CarRepositoryImpl carRepository;
+
+    public SearchServiceImpl(CarRepositoryImpl carRepository) {
+
+        this.carRepository = carRepository;
+    }
+
+    /**
+     * Public method findCarsByMake searches cars by make.
+     *
+     * @param make holds value of car make
+     * @return list of found cars.
+     */
+    public List<Car> findCarsByMake(String make) {
+        List<Car> foundCars = new ArrayList<Car>();
+
+        for (Car car : carRepository.getAll()) {
+            if (car.getMake().equalsIgnoreCase(make)) {
+                foundCars.add(car);
+            }
+        }
+
+        return foundCars;
+    }
+
+    /**
+     * Public method findCarsByMakeAndModel searches cars by make and model.
+     *
+     * @param make  holds value of car make
+     * @param model holds value f car model
+     * @return list of found cars.
+     */
+    public List<Car> findCarsByMakeAndModel(String make, String model) {
+        List<Car> foundCars = new ArrayList<Car>();
+
+        for (Car car : carRepository.getAll()) {
+            if ((car.getModel().equalsIgnoreCase(model)) && (car.getMake().equalsIgnoreCase(make))) {
+                foundCars.add(car);
+            }
+        }
+
+        return foundCars;
+    }
+
+    /**
+     * Public method findCarsByMultipleCategories searches cars by make, model, color, seats.
+     *
+     * @param make  value of car make
+     * @param model value of car model
+     * @param color value of car color
+     * @param seats value of car seats
+     * @return list of found cars
+     */
+    public List<Car> findCarsByMultipleCategories(String make, String model, String color, int seats) {
+        List<Car> foundCars = new ArrayList<Car>();
+
+        for (Car car : carRepository.getAll()) {
+            if ((car.getModel().equalsIgnoreCase(model)) && (car.getMake().equalsIgnoreCase(make))
+                    && (car.getColor().equalsIgnoreCase(color)) && (car.getSeats() == seats)) {
+                foundCars.add(car);
+            }
+        }
+
+        return foundCars;
+    }
+
+    public CarRepositoryImpl getCarRepository() {
+        return carRepository;
+    }
+
+}
